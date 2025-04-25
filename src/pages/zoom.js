d187f47002c33c196f36342d8303c74c1593325f
not2nder <brtiodark@gmail.com>
2025-04-25 16:00:31 -0300

    regret

--- src/pages/zoom.js
+++ src/pages/zoom.js
@@ -11,45 +11,38 @@ document.querySelector('#app').innerHTML = `
     <span class="visually-hidden">Carregando...</span>
   </div>
 </div>
-
-<div class="page d-flex flex-column min-vh-100" id="conteudo">
-  <main class="container px-3 flex-grow-1">
-    <div class="row">
-      <!-- Coluna da esquerda com a imagem e interações -->
-      <div class="col-md-6 mb-4">
-        <div class="card p-4 d-flex flex-column justify-content-between h-100">
-          <div class="img-wrapper mb-3" id="img-wrapper">
-            <img id="capa" alt="Capa do álbum">
-            <div id="resposta" class="resposta-overlay d-none">Resposta</div>
-          </div>
-          <div>
-            <div class="d-flex align-items-center">
-              <span>Tentativas Restantes:</span>
-              <ul id="vidas" class="d-flex px-2 m-0"></ul>
-            </div>
-            <button type="button" class="btn btn-success mt-3 w-100" style="display: none" id="proximo-album">
-              <i class="bi bi-arrow-right"></i>
-            </button>
-          </div>
+<div id="conteudo" style="display: none;" class="page d-flex flex-column min-vh-100">
+  <main class="container px-3 flex-grow-1 py-2">
+    <div class="left-side w-100">
+      <div class="card p-3">
+        <div class="img-wrapper" id="img-wrapper">
+          <img id="capa" alt="Capa do álbum">
+          <div id="resposta" class="resposta-overlay d-none">Resposta</div>
         </div>
+        <div class="mt-2 d-flex align-items-center">
+          <span>Tentativas Restantes: </span>
+          <ul id="vidas" class="d-flex px-2 m-0"></ul>
+        </div>
+        <button type="button" class="btn btn-success mt-2" style="display: none" id="proximo-album">
+          <i class="bi bi-arrow-right"></i>
+        </button>
       </div>
+    </div>
 
-      <!-- Coluna da direita com os seletores e tentativas -->
-      <div class="col-md-6">
-        <div class="card p-4 mb-3">
-          <span>Nome do Álbum:</span>
-          <select id="album-select" class="tom-select" placeholder="🔎 Selecione um álbum..."></select>
-        </div>
-        <div class="card p-4 chutes">
-          Seus chutes:
-          <ul id="tentativas" class="list-group mt-2"></ul>
-        </div>
+    <div class="right-side w-100">
+      <div class="card p-3">
+        <span>Nome do Álbum:</span>
+        <select id="album-select" class="tom-select" placeholder="🔎 Selecione um álbum...">
+        </select>
+      </div> 
+      <div class="card p-3 mt-2 chutes">
+        Seus chutes: 
+        <ul id="tentativas" class="list-group"></ul>
       </div>
     </div>
   </main>
-
-  <footer class="d-flex flex-column align-items-center p-3 mt-auto">
-    <div class="d-flex align-items-center flex-column text-center">
+  <footer class="d-flex flex-column align-items-center p-3">
+    <div class="d-flex align-items-center flex-column">
       <span>Desenvolvido por not2nder. Dados pela <a target="_blank" href="https://developer.spotify.com/documentation/web-api">API do Spotify</a></span>
       <span>© 2025 NOT2NDER</span>
     </div>
