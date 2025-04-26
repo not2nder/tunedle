e5fed12442981038b98c57d89a0faeed07dc6e59
not2nder <brtiodark@gmail.com>
2025-04-26 14:32:25 -0300

    main screen text

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,9 @@ import TomSelect from 'tom-select';
 document.querySelector('#app').innerHTML = `
   <div class="page d-flex flex-column min-vh-100" style="background: linear-gradient(45deg,rgba(139, 206, 151, 1) 0%, rgba(26, 123, 127, 1) 41%, rgba(6, 70, 107, 1) 100%)">
     <main class="page-container px-3 flex-grow-1">
+      <div>
+        <h1 class="pulse main-text" id="text-pulse"></h1>
+      </div>
       <div class="row">
         <div class="col-md-6 mb-4">
           <div class="card card-hover p-4 d-flex flex-column justify-content-between">
@@ -46,16 +49,47 @@ document.querySelector('#app').innerHTML = `
       </div>
     </main>
 
-    <footer class="d-flex flex-column align-items-center p-3 mt-auto">
-      <div class="d-flex align-items-center flex-column text-center">
-        <span>Desenvolvido por not2nder. Dados da <a target="_blank" href="https://developer.spotify.com/documentation/web-api">API do Spotify</a></span>
-        <span>© 2025 NOT2NDER</span>
+    <footer class="footer mt-auto py-3">
+      <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div>
+          <span>© 2025 NOT2NDER</span>
+        </div>
+        <div>
+          <a href="https://developer.spotify.com/documentation/web-api" target="_blank">API do Spotify</a>
+        </div>
+        <div class="social-icons mt-2 mt-md-0">
+          <a href="https://github.com/not2nder"><i class="bi bi-github"></i></a>
+          <a href="https://instagram.com/2nder.dev"><i class="bi bi-instagram"></i></a>
+          <a href="https://tiktok.com/@not2nder"><i class="bi bi-tiktok"></i></a>
+        </div>
       </div>
     </footer>
   </div>
 `;
 
 const select = document.getElementById('artist-select');
+
+const frases = [
+  "O som está nos detalhes",
+  "Quantos álbuns você conhece de verdade?",
+  "Treine seus ouvidos e sua memória!",
+  "Quando a capa diz tudo — ou quase tudo",
+  "Errar também faz parte da música",
+  "Nem sempre é tão fácil quanto parece!",
+  "Feito com bugs, amor e muita música.",
+  "Este jogo teve mais versões que álbuns remasterizados.",
+  "Atualizações frequentes... igual playlist de fim de semana.",
+  "Descubra, adivinhe, desafine!",
+  "Alguns álbuns envelhecem como vinho... outros como leite",
+  "Nem toda capa famosa é tão fácil de reconhecer!",
+  "Um dia sem música é um dia perdido",
+  "Jogue com seu artista favorito!",
+  "Mostre que você conhece cada álbum!",
+  "O próximo hit pode estar a um follow de distância.",
+  "Siga o desenvolvedor"
+];
+
+document.getElementById('text-pulse').textContent = frases[Math.floor(Math.random() * frases.length)];
 
 new TomSelect(select, {
     valueField: 'id',
