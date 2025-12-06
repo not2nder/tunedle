4515f9ca02c01a782f8c042429366ddee9b3345b
not2nder <brtiodark@gmail.com>
2025-12-05 21:02:23 -0300

    Ultima mudança do dia

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,59 +7,55 @@ import TomSelect from 'tom-select';
 document.querySelector('#app').innerHTML = /*html*/`
 <header class="w-100 py-4 px-5 d-flex align-items-center justify-content-between">
   <h2>🎵 Tunedle</h2>
-
-  <nav class="d-flex gap-3">
-    <a href="https://instagram.com/not2nder" class="text-white fw-semibold">Contato</a>
-  </nav>
 </header>
 
-<div class="background">
-  <div class="page d-flex flex-column min-vh-100">
-    <main class="page-container px-3 flex-grow-1">
-      <div>
-        <h1 class="pulse main-text" id="text-pulse"></h1>
-      </div>
-      <div class="row">
-        <div class="col-md-6 mb-4">
-          <div class="card card-hover p-4 h-100">
-            <h4 class="mb-3"><i class="fa-solid fa-gamepad"></i> Modos de Jogo</h4>
-            <div class="py-3">
-              <span>Antes de selecionar um modo, escolha um artista</span>
-              <select id="artist-select" class="tom-select" placeholder="🔎 Procure um Artista"></select>
-            </div>
-
-            <ul class="p-0">
-              <li class="mb-3">
-                <button class="btn w-100 button easy" id="modo-zoom" role="button">
-                  <strong>MODO ZOOM 🔎</strong>
-                  <span class="small">Descubra o álbum pela capa</span>
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="col-md-6 mb-4">
-          <div class="card card-hover p-4 d-flex flex-column justify-content-between">
-            <div>
-              <h3><i class="fa-solid fa-music"></i> Tunedle</h3>
-              <p>
-                Tente adivinhar o nome de um álbum apenas olhando a capa — quanto mais rápido, melhor!
-                No <strong>Modo Zoom</strong>, a imagem é revelada aos poucos... consegue acertar antes de ver tudo?
-              </p>
-              <p> Mais modos em breve!</p>
-              <p class="text-muted small">Versão 1.0 • Desenvolvido por <strong>not2nder</strong>.</p>
-            </div>
-            <span>Apoie o Desenvolvedor:</span>
-            <div>
-              <a href="https://github.com/not2nder"><i class="fa-brands fa-github"></i></a>
-              <a href="https://instagram.com/not2nder"><i class="fa-brands fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+<section id="homeContainer" class="page-container d-flex flex-column min-vh-100 px-3 flex-grow-1">
+  <div class="my-4">
+      <h1 class="pulse main-text mb-4 text-center" id="text-pulse"></h1>
   </div>
+
+  <div class="row justify-content-center">
+
+    <!-- MODO DE JOGO -->
+    <div class="col-md-6 mb-4">
+      <div class="card card-hover p-4 h-100">
+
+        <h4 class="mb-3">
+          <i class="fa-solid fa-gamepad"></i> Modos de Jogo
+        </h4>
+
+        <p class="mb-2">Escolha um artista para começar:</p>
+
+        <select id="artist-select" class="tom-select"
+                placeholder="🔎 Procure um Artista"></select>
+
+        <button class="btn w-100 button easy mt-4" id="modo-zoom">
+          <strong>MODO ZOOM 🔎</strong>
+          <span class="small d-block">Descubra o álbum pela capa</span>
+        </button>
+
+      </div>
+    </div>
+    <div class="col-md-6 mb-4">
+      <div class="card card-hover p-4 d-flex flex-column justify-content-between">
+        <div>
+          <h3><i class="fa-solid fa-music"></i> Tunedle</h3>
+          <p>
+            Tente adivinhar o nome de um álbum apenas olhando a capa — quanto mais rápido, melhor!
+            No <strong>Modo Zoom</strong>, a imagem é revelada aos poucos... consegue acertar antes de ver tudo?
+          </p>
+          <p> Mais modos em breve!</p>
+          <p class="text-muted small">Versão 1.0 • Desenvolvido por <strong>not2nder</strong>.</p>
+        </div>
+        <span>Apoie o Desenvolvedor:</span>
+        <div>
+          <a href="https://github.com/not2nder" target="_blank"><i class="fa-brands fa-github"></i></a>
+          <a href="https://instagram.com/not2nder" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 `;
 
 const select = document.getElementById('artist-select');
